083f598e2f3257ce0725a0ed37d19af60275283f
Luis Garcia <luisgarcia26284@gmail.com>
2022-03-09 14:31:55 +0800

    Draft of q1

--- hw2/ottersome/q1/main.cpp
+++ hw2/ottersome/q1/main.cpp
@@ -1,42 +1,79 @@
 #include <vector>
+#include <iostream>
 
 class Line{
     public:
-        Line(){
-            //I guess this one can initialize an empty vector
+        Line(){ //I guess this one can initialize an empty vector
             x_vec.clear();
             y_vec.clear();
         }
         //Copy Constructor
-        Line(Line const & ){
-            auto xvec_it = x_vec.begin()
-            auto yvec_it = y_vec.begin()
-
+        Line(Line const & old_line){
+            for(size_t i = 0;i<old_line.size();i++){
+                x_vec.push_back(old_line.x(i));
+                y_vec.push_back(old_line.y(i));
+            }
         }
-        //Move Constructor
-        Line(Line       &&);
+        //Move Constructor(TODO)
+        Line(Line && old_line){
+            x_vec = std::move(old_line.all_x());
+            y_vec = std::move(old_line.all_y());
+        }
         //Copy Assignment
-        Line & operator=(Line const & );
+        Line & operator=(Line const & old_line){
+            for(size_t i = 0;i<old_line.size();i++){
+                x_vec.push_back(old_line.x(i));
+                y_vec.push_back(old_line.y(i));
+            }
+            return *this;
+        }
+        std::vector<float> & all_x(){
+            return this->x_vec;
+        }
+        std::vector<float> & all_y(){
+            return this->y_vec;
+        }
         //Move Assignment
-        Line & operator=(Line       &&);
+        Line & operator=(Line && old_line){
+            //Assign entire vectors maybe ?
+            x_vec = std::move(old_line.all_x());
+            y_vec = std::move(old_line.all_y());
+            return *this;
+        }
         //Simple Allocation
-        Line(size_t size);
+        Line(size_t size){
+            x_vec.resize(size);
+            y_vec.resize(size);
+        }
         //Destructor
-        ~Line();
+        ~Line(){
+            //Not necessary I believe but still leaving it here
+            x_vec.clear();
+            y_vec.clear();
+        }
         //Get current size 
-        size_t size() const;
-        float & x(size_t it) const;
-        float & x(size_t it);
-        float & y(size_t it) const;
-        float & y(size_t it);
+        size_t size() const{
+            return x_vec.size();
+        }
+        float & x(size_t it) const{
+            return x_vec[it];
+        }
+        float & x(size_t it){
+            return x_vec[it];
+        }
+        float & y(size_t it) const{
+            return y_vec[it];
+        }
+        float & y(size_t it){
+            return y_vec[it];
+        }
 
     private:
         //Lets keep an unitialized vector here
-        std::vector<int> x_vec;
-        std::vector<int> y_vec;
+        mutable std::vector<float> x_vec;
+        mutable std::vector<float> y_vec;
 
 };
-
 int main(int, char **)
 {
     Line line(3);
